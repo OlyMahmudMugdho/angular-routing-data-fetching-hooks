ca5979b515a8332375467a4721f0eb7e0ebc53cd
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-16 17:51:08 +0600

    protected routes (guards) - logout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -27,7 +27,7 @@ export const routes: Routes = [
     component : ParentComponent
   },
   {
-    path : 'login',
+    path : 'auth',
     component : LoginComponent
   },
   {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,37 @@
 import {Component, inject} from '@angular/core';
 import {AuthService} from "../service/auth/auth.service";
 import {Router} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   authService : AuthService;
   router = inject(Router);
+
+  loggedIn !: boolean;
+
   constructor(authService : AuthService) {
-    this.authService = authService
+    this.authService = authService;
+    this.loggedIn = authService.isLoggedIn();
   }
+
 
   doLogin() {
     this.authService.login();
     this.router.navigate(["/protected"])
   }
+
+  doLogOut() {
+    this.authService.logout();
+    this.router.navigate(["/"])
+  }
+
 }
